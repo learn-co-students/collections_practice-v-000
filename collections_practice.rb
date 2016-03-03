2706ce533a466cdcbac8e347082d3ca29aa85355
tddycks <tadzio.smith@gmail.com>
2016-03-02 21:09:25 -0500

    Done.

---
+++ collections_practice.rb
@@ -0,0 +1,73 @@
+def sort_array_asc(array)
+  array.sort 
+
+end
+
+
+def sort_array_desc(array)
+  array.sort { |a, b|
+    b <=> a
+  }
+
+end 
+
+
+def sort_array_char_count(array)
+  array.sort { |a, b|
+    a.length <=> b.length
+  }
+
+end
+
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+
+end
+
+
+def swap_elements_from_to(array, index, destination)
+  array[index] = array[destination]
+end
+
+
+
+def reverse_array(array)
+  array.reverse
+end
+
+
+def kesha_maker(array)
+  array.each do |word|
+    word[2] = "$"
+  end
+end
+
+
+
+def find_a(array)
+  array.select { |word|
+    word.start_with?("a")
+  }
+end
+
+
+def sum_array(array)
+  array.inject { |sum, element| sum + element}
+end
+
+
+def add_s(array)
+    array.each_with_index {|element, index|
+      if index != 1
+      element << "s"
+     elsif index == 1
+      element
+        
+      end
+    }
+end
+
+
+
