d890677de2bf9e1fa9d9f2e81c82927102eefef3
zszalas2 <zszalas@gmail.com>
2016-03-13 14:34:56 -0400

    long one but a goodie

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -29,6 +29,14 @@ describe 'collections practice' do
     end
   end
 
+  #advacned question
+
+  describe '#swap_elements_from_to' do
+    it 'swap the second and third elements of an array' do
+      expect(swap_elements_from_to(["blake", "ashley", "scott"], x = 1 , y = 2)).to eq(["blake", "scott", "ashley"])
+    end
+  end
+
   # Question 4
   describe '#reverse_array' do
     it 'reverse the order of an array' do
