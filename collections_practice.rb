199dc4da5b3175acb927c4e65bd9ed3b5d3b30a0
Certain125 <amanita.certain@gmail.com>
2016-04-03 22:27:08 -0400

    Done.

---
+++ collections_practice.rb
@@ -0,0 +1,18 @@
+
+
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array = []
+  array.sort {|word| word.size}
+  return 
+end
+
+def swap_elements(array)
+end
