61a8c483c140a7fab9aecb202bb32b1415ea29da
cmcmcmcm <ninehundredpx@gmail.com>
2016-04-13 19:52:57 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -29,11 +29,33 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  array.sort do |a, b|
-    if a.index==0
-      0
-    elsif a.index==1 && b.index==2
-      1
-    end
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  rev_array=[]
+  array.each { |a| rev_array.insert(0, a) }
+  rev_array
+end
+
+def kesha_maker(array)
+  kesha_array=[]
+  array.each do |a|
+    kesha_array << a.gsub(a[2], "$")
   end
+  kesha_array
+end
+
+def find_a(array)
+  array.select { |word| word.start_with?("a") }
+end
+
+def sum_array(array)
+  sums = 0
+  array.each { |a| sums += a }
+  sums
+end
+
+def add_s(array)
+  array.each_with_index.collect { |element, index| index == 1 ? element : element + "s" }
 end
