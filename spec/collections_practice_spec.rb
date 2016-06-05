6c53308587b1d589e54221b35270ef3f8fa129ef
Jennifer Dixon <jenniration@gmail.com>
2016-06-05 15:20:35 -0400

    Done.

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -29,6 +29,12 @@ describe 'collections practice' do
     end
   end
 
+  describe '#swap_elements_from_to' do
+    it 'swap elements from index to destination' do
+      expect(swap_elements_from_to(["blake", "ashley", "scott"],2,0)).to eq(["scott", "ashley", "blake"])
+    end
+  end
+
   # Question 4
   describe '#reverse_array' do
     it 'reverse the order of an array' do
