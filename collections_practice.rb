10dee124a12e58bdd7fa5ff2dbf859135004e3ae
Traci7822 <tracir@gmail.com>
2016-06-16 13:17:50 -0600

    Done.

---
+++ collections_practice.rb
@@ -0,0 +1,59 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+#["blake", "scott", "ashley"]
+  name = array[1]
+  array[1] = array[2]
+  array[2] = name
+  array
+end
+
+def reverse_array(array)
+  array.reverse!
+end
+
+def kesha_maker(array)
+  array.each do |name|
+    name[2] = "$"
+end
+end
+
+def find_a(array)
+  collection = []
+  array.each do |word|
+    if word.start_with?("a")
+      collection << word
+    end
+  end
+  collection
+end
+
+def sum_array(array)
+  array.inject(0) {|result, element| result + element }
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index == 1 
+      element
+    else
+      "#{element}s"
+    end #if statment
+  end #each
+end#method
