356de5ae3e79974f7f45b061a34e5b104d8c182c
Andres Duarte <andresduarte010@gmail.com>
2016-10-12 17:18:14 +0000

    Done.

---
+++ collections_practice.rb
@@ -0,0 +1,42 @@
+def sort_array_asc(array)
+  array.sort {|a, b| a <=> b}
+end
+
+def sort_array_desc(array)
+  array.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort { |a, b| a.length - b.length }
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array(array)
+  modi = array.reverse
+  return modi
+end
+
+def kesha_maker(array)
+  array.each {|e| e[2] = '$'}
+end
+
+def find_a(array)
+  array.select {|e| e.start_with?('a')}
+end
+
+def sum_array(array)
+  array.inject {|result, element| result + element}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if !(index == 1)
+      element << "s"
+    end
+      element
+  end
+end
