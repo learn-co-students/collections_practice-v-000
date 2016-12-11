365f30fa8c2135f82babf7470334f845891862d7
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2016-12-11 03:26:11 +0000

    Done.

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -29,6 +29,15 @@ describe 'collections practice' do
     end
   end
 
+  describe "#swap_elements_from_to" do
+    it "swap the indS and indF elements of an array 1" do
+      expect(swap_elements_from_to(["a", "b", "c"],0,2)).to eq(["c", "b", "a"])
+    end
+    it "swap the indS and indF elements of an array 2" do
+      expect(swap_elements_from_to(["a", "b", "c"],2,1)).to eq(["a", "c", "b"])
+    end
+  end
+
   # Question 4
   describe '#reverse_array' do
     it 'reverse the order of an array' do
