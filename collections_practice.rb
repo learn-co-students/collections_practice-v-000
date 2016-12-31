69245605a78fdcbe2f846076b4dfe2daccc3f983
charlie lee <ctlee9096@gmail.com>
2016-12-31 17:14:29 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -24,8 +24,44 @@ def reverse_array(array)
 array[0], array[1], array[2] = array [2], array [1], array[0]
 array
 end
-
-
+def kesha_maker(array)
+  n1 = array.length
+  i = 0
+  while i < n1
+  array.each do |item| 
+    #binding.pry
+    n= item.length
+    #binding.pry
+    i = 0
+    while i < n 
+      #binding.pry
+      item[2] = item[2].replace ("$")
+      #binding.pry
+      i = i + 1
+    end
+    array
+    #binding.pry
+    end 
+  end
+    return array
+end
+#http://stackoverflow.com/questions/31654885/change-every-nth-letter-in-an-array
+#build a method `find_a` that returns all the strings 
+#in the array passed to it that `start_with?` (**hint**) the letter `"a"`.
+def find_a(array)
+  n = length of array
+  i = 0
+  new_a_array = [] # to start a collection
+  while i < n #counter is less than length of array do the following
+    array.select do |item| 
+      while item[0]=="a"
+        new_a_array<< item
+      end
+      i= i + 1
+      end
+  end
+new_a_array
+end
 
 
   
