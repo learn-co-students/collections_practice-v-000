17272bc3a8115e2e1eee23f47d3ec7d3bb73f3a0
Jonathan Foster <redcabinny@gmail.com>
2017-01-05 04:38:54 +0000

    Done.

---
+++ collections_practice.rb
@@ -0,0 +1,75 @@
+def sort_array_asc(arr)
+  arr.sort
+end
+
+def sort_array_desc(arr)
+  arr.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+  end
+end
+
+def sort_array_char_count(arr)
+  arr.sort do |a, b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
+end
+
+def swap_elements(arr)
+  temp = arr[2]
+  arr[2] = arr[1]
+  arr[1] = temp
+  arr
+end
+
+def swap_elements_from_to(arr, index, destination_index)
+  temp = arr[destination_index]
+  arr[destination_index] = arr[index]
+  arr[index] = temp
+  arr
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr)
+  arr.collect do |name|
+    name.slice(0, 2) + "$" + name.slice(3, name.length - 3)
+  end
+end
+
+def find_a (arr)
+  arr.select do |name|
+    name[0] == "a"
+  end
+end
+
+def sum_array(arr)
+  answer = 0
+  arr.each do |num|
+    answer += num
+  end
+  answer
+end
+
+def add_s(arr)
+  arr.collect.each_with_index do |word, index|
+    if index != 1
+      word + "s"
+    else
+      word
+    end
+  end
+end
--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -29,6 +29,12 @@ describe 'collections practice' do
     end
   end
 
+  describe '#swap_elements_from_to' do
+    it 'swap the entered elements of an array' do
+      expect(swap_elements_from_to(["a", "b", "c"], 0, 2)).to eq(["c", "b", "a"])
+    end
+  end
+
   # Question 4
   describe '#reverse_array' do
     it 'reverse the order of an array' do
