da2d415101644eb06fc8deb0f2833baeae3ccf1d
Catherine Cordova <ccordova41@gmail.com>
2017-01-24 16:09:48 +0000

    Done.

---
+++ collections_practice.rb
@@ -0,0 +1,42 @@
+def sort_array_asc(num)
+  num.sort
+end
+
+def sort_array_desc(num)
+  num.sort.reverse
+end
+
+def sort_array_char_count(num)
+  num.sort_by {|word| word.length}
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  return array.reverse
+end
+
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |words| words[2] = '$'
+  new_array << words
+  end
+  return new_array
+end
+
+def find_a(array)
+
+  array.find_all{|snack|snack.start_with?("a")}
+  end
+
+def sum_array(num)
+  num.inject {|sum, n| sum + n }
+end
+
+def add_s(array)
+    array = array.each_with_index.collect { |item, index| item + "s"}
+    array = array[0], array[1].chomp('s'), array[2], array[3]
+end
