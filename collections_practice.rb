aad524591f2f1d3ee32a2ba6935a6d798fcad4f3
Eric Levy <levyeric10@gmail.com>
2017-01-31 22:58:08 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -23,3 +23,29 @@ end
 def reverse_array(array)
   array.reverse
 end
+
+def kesha_maker(array)
+  array.each do |character|
+    character[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select do |string|
+    string.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject {|result, element| result + element}
+end
+
+def add_s(array)
+  array.collect do |string|
+    if string == array[1]
+      string
+    else
+      string << "s"
+    end
+  end
+end
