a370cde38b76306a3518c2abc7c34f7f35501e7d
Michael Etlinger <mk.etlinger@gmail.com>
2017-03-03 19:34:37 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
+def sort_array_asc(array)
+ascending_array =  array.sort
+end
 
+def sort_array_desc(array)
+  descending_array = array.sort { |a, b| b <=> a }
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.length <=> b.length }
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = array.each { |e| e.slice!(2) + e.insert(2,'$') }
+end
+
+def find_a(array)
+  array.select {|e| e.start_with?('a')}
+end
+
+def sum_array(array)
+  array.inject {|sum, n| sum + n}
+end
+
+def add_s(array)
+  array.map {|word| word == array[1] ? word : word + 's' }
+end
