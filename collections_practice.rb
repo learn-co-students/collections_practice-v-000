55e90e88dc40a7990447e2eb8345572cf60d656c
thesilentc <thesilentc@me.com>
2017-03-08 17:41:41 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,3 +22,15 @@ end
 def kesha_maker(array)
   array.each {|item|item[2] = "$"}
 end
+
+def find_a(array)
+  array.select { |string| string[0] == "a"}
+end
+
+def sum_array(array)
+  sum = 0
+  array.each do |num|
+    sum+=num
+  end
+  sum
+end
