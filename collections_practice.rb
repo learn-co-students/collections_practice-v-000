2851f58c922a5a5752ddb22f715df858002af4d2
John Ziegner <jziegner23@gmail.com>
2017-03-10 07:47:43 +0000

    Done

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,51 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do | a, b |
+    if a.length  == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
+end
+
+def swap_elements(array)
+  array.sort {|x, y| x[1] <=> y[2]}
+end
+
+def reverse_array(array)
+  reversal = array.reverse
+end
+
+def kesha_maker(array)
+  array.each do | string |
+    string[2] = "$"
+  end
+end
+
+def find_a(array)
+    array.select{|word| word.start_with?("a")}
+end
+
+def sum_array(array)
+    array.inject(:+)
+end
+
+def add_s(array)
+  array.collect do | string |
+    if array[1] == string
+     string
+    else
+      string + "s"
+    end
+  end
+end
