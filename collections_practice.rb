7219f55946256c07f5b38441dcc0f1ca67252d8e
antley chu <antley.chu@gmail.com>
2017-03-20 17:01:18 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,64 @@
 
+def sort_array_asc(array_of_integers)
+  array_of_integers.sort
+end
+
+def sort_array_desc(array)
+  array.sort do |a,b|
+   if a == b
+      0
+   elsif a < b
+      1
+   elsif a > b
+      -1
+   end
+ end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.collect do |item|
+    item[2] = "$"
+  end
+  array
+end
+
+def find_a(array)
+  array.select{ |i| i[0]=="a"}
+end
+
+def sum_array(array)
+  array.inject {|sum,n| sum + n}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index == 1
+      element
+    else
+      element << 's'
+    end
+  end
+
+end
