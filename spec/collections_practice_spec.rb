820ea9ea6d284d79936b3c6ba4dfb39031eb7f24
Jennifer Haddock <jentx123@hotmail.com>
2017-03-25 19:07:38 +0000

    Done.

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,13 @@ describe 'collections practice' do
     end
   end
 
+  #Question 4B
+  describe '#swap_elements_from_to' do
+   it 'swap the array elements for the indicies given' do
+     expect(swap_elements_from_to(["a", "b", "c"],0,2)).to eq(["c", "b", "a"])
+   end
+  end
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array' do
