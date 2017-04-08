d15de07b26c105abdd307e22ca7bdc43d41a1646
Christina Varipapa <christina.varipapa@gmail.com>
2017-04-08 22:05:29 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,57 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+=begin
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+=end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def swap_elements_from_to(array, index, destination_index)
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = array.each do |string|
+    string[2] = '$'
+  end
+  new_array
+end
+
+def find_a(array)
+  array.find_all {|word| word[0] == "a" }
+end
+
+def sum_array(array)
+  array.reduce(0,:+)
+end
+
+def add_s(array)
+  array.map do |word|
+    if word != array[1]
+      word += 's'
+    else word
+    end
+  end
+end
