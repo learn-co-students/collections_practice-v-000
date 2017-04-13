b70338173edb5db59e25103a9c80cc5cf0c0bfab
Nick Bair <bairinc@gmail.com>
2017-04-13 17:38:47 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
+def sort_array_asc(arr)
+  arr.sort
+end
 
+def sort_array_desc(arr)
+  arr.sort { |a, b| b <=> a }
+end
+
+def sort_array_char_count(arr)
+  arr.sort { |a, b| a.length <=> b.length }
+end
+
+def swap_elements(arr)
+  arr.insert(1, arr[2]).delete_at(3)
+  return arr
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr)
+  result = []
+  arr.each do |s|
+    s[2] = "$"
+    result << s
+  end
+end
+
+def find_a(arr)
+  arr.keep_if { |s| s.start_with?("a") }
+end
+
+def sum_array(arr)
+  arr.reduce(:+)
+end
+
+def add_s(arr)
+  arr.each_with_index.collect { |element, index| index != 1 ? element += "s" : element }
+end
