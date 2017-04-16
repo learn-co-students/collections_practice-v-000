7323c3fb88ba2abe64a6a2d53a0deb4a8931d3dc
Aly Alidina <aly_alidina@hotmail.com>
2017-04-16 17:35:52 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
+def sort_array_asc (integers)#1
+  copy = integers.sort
+  copy
+end
 
+def sort_array_desc(integers)#2
+  copy = integers.sort {|a,b| b <=> a}
+  copy
+end
+
+def sort_array_char_count(words)#3
+  copy = words.sort {|a,b| a.upcase <=> b.upcase}
+  copy
+end
+
+def swap_elements(words)#4
+  arr = []
+  words.each_with_each do |val, index|
+    arr << val[1], val[2] = val[2], val[1]
+  end
+  arr
+end
+
+def reverse_array(position)#5
+  position.sort {|swap| swap[1] <=> swap[2]}
+end
+
+def kesha_maker(words) #6
+  words.each do |val, index|
+    x.replaces
+  end
+end
+
+
+
+def find_a() #7
+
+end
