1bd9146300fa8ab4dab2eefae6bff921d91e0222
Heather You <heatherhyou@gmail.com>
2017-04-19 02:17:21 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,6 +6,12 @@ def sort_array_desc(array)
   array.sort.reverse
 end
 
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
 def swap_elements(array)
 
 end
@@ -23,3 +29,13 @@ def kesha_maker(array)
 end
 
 def find_a(array)
+
+end
+
+def sum_array(array)
+
+end
+
+def add_s(array)
+
+end
