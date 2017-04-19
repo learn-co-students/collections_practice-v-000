043892f8ba07dddf1331c71635cf54c88ba22e3f
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-04-19 15:39:31 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
+require 'pry'
 
+def sort_array_asc(integers)
+  integers.sort
+end
+
+def sort_array_desc(integers)
+  integers.sort.reverse
+end
+
+def sort_array_char_count(strings)
+  strings.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+new_array = []
+  array.each do |a|
+    sub_array = a.split
+    sub_array.map { |e| e[2] = "$" }
+    new_array << sub_array.join
+  end
+ new_array
+end
+
+def find_a(array)
+  
+end
