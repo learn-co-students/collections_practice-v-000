b51374453ecf17254beae221d8d80ea3389a3928
Heather You <heatherhyou@gmail.com>
2017-04-19 00:53:22 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,25 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def swap_elements(array)
+
+end
+
+def swap_elements_from_to(array, index, destination_index)
+
+end
+
+def reverse_array(array)
+
+end
+
+def kesha_maker(array)
+
+end
+
+def find_a(array)
