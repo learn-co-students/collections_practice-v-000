51716a1184c0632df81b560b0af09ce74e4ef33b
Deborah Johnson <debjohnson_11@yahoo.com>
2017-04-19 14:10:40 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
+def sort_array_asc(integers)
+    integers.sort
+end
 
+def sort_array_desc(integers)
+    integers.sort {|x, y| y <=> x}
+end
+
+def sort_array_char_count(strings)
+    strings.sort {|left, right| left.length <=> right.length}
+end
+
+def swap_elements(array)
+
+end
