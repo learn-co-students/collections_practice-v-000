5d833ad3f42d3fc05b7c96ef7d57e92edd0a587e
Alfredo Urdaneta <alfredo.i.urdaneta@gmail.com>
2017-04-23 08:50:49 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort { |a, b| a.length <=> b.length }
+end
+
+def swap_elements(array)
+  x = array[1]
+  array[1] = array[2]
+  array[2] = x
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  kesha = []
+  array.each do |text|
+    text[2] = "$"
+    kesha << text
+  end
+  kesha
+end
+
+def find_a(array)
+  array.select { |item| item.start_with?("a") }
+end
+
+def sum_array(array)
+  array.reduce(:+)
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index != 1
+      element << "s"
+    else
+      element
+    end
+  end
+end
