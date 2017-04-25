8ebe77a711e0090c04d5661e6d8287bc4d0b0e69
Stephen Harrington <stephenharrington04@yahoo.com>
2017-04-25 18:03:16 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,87 @@
 
+def sort_array_asc(numbers)
+  numbers.sort
+end
+
+def sort_array_desc(numbers)
+  numbers.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+  end
+end
+
+def sort_array_char_count(strings)
+  strings.sort do |a, b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
+end
+
+def swap_elements(array)
+  a = array[1]
+  b = array[2]
+  array[1] = b
+  array[2] = a
+  array
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  a = array[index]
+  b = array[destination_index]
+  array[index] = b
+  array[destination_index] = a
+  array
+end
+
+def reverse_array(array)
+  new_array = []
+  array.each do |number|
+    new_array.unshift(number)
+  end
+  new_array
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |string|
+    string[2] = "$"
+    new_array << string
+  end
+  new_array
+end
+
+def find_a(array)
+  array.select do |string|
+    string.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  #sum = 0
+  #array.each do |number|
+  #  sum += number
+  #end
+  #sum
+  array.inject do |sum, n|
+    sum + n
+  end
+end
+
+def add_s(array)
+  array.each_with_index.collect do |string, index|
+    if index != 1
+      string << "s"
+    else string
+    end
+  end
+end
