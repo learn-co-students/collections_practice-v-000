49079e3a3888d618bcfa85b49b7eaea00f05bd8f
Omar Donnadieu Mercado <donnadieu.86@gmail.com>
2017-04-28 17:07:41 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(array)
+  array.sort { |x,y| x <=> y }
+end
 
+def sort_array_desc(array)
+  array.sort { |x,y| y <=> x }
+end
+
+def sort_array_char_count(array)
+  array.sort { |x,y| x.length <=> y.length }
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |word|
+    word[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select { |word| word[0] == "a" }
+end
+
+def sum_array(array)
+  array.inject { |sum, x| sum + x}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index != 1
+      element + "s"
+    else
+      array[1] = array[1]
+    end
+  end
+end
