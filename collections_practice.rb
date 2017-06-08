f47f4c0c30698f204ea7205d110c66e5a52993be
Meredith Reece <nerdybug@gmail.com>
2017-06-08 00:39:12 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,46 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each { |item| item[2] = "$" }
+end
+
+def find_a(array)
+  array.find_all { |item| item[0] == "a" }
+end
+
+def sum_array(array)
+  total = 0
+  array.each { |n| total = n + total }
+  total
+end
+
+def add_s(array)
+  array.each do |item|
+    unless item == array[1]
+      item << "s"
+    end
+  end
+end
