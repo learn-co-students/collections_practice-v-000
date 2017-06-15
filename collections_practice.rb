5f30bf90ddbade039abede25cc967d47dba0ede7
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2017-06-15 03:09:37 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|a,b| b <=> a }
+end
+
+def sort_array_char_count(array)
+  array.sort { |a,b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+    }
+end
+
+def swap_elements(array)
+  array.sort {|a,b|
+    if a == array[1] && b == array[2]
+      1
+    else
+      0
+    end
+  }
+end
+
+def reverse_array(array)
+ array.reverse
+end
+
+def kesha_maker(array)
+    new_array = []
+    array.each { |a|
+    new_array << a.gsub(a[2],"$")
+    }
+    new_array
+end
+
+def find_a(array)
+  array.select {|a| a[0] == "a" }
+end
+
+def sum_array(array)
+  array.inject {|sum, num| sum + num}
+end
+
+def add_s(array)
+array.each_with_index.collect{|element, index| if index != 1 then "#{element}" + "s" else "#{element}"  end}
+end
