dfe1cafa909036fb15cbf73ac46824c9e19a3deb
Robin Doirin <robindoirin@gmail.com>
2017-06-28 18:48:20 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,51 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+ def sort_array_desc(array)
+   array.sort do |a, b|
+     b <=> a
+  end
+ end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array [2], array[1]
+  array
+end
+
+def reverse_array(arr)
+  arr.reverse
+
+end
+
+def kesha_maker(arr)
+  arr.each do |kesha|
+    kesha[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select do |x|
+    x.start_with?("a")
+  end
+end
+
+def sum_array(arr)
+  arr.inject {|a, b| a + b}
+end
+ def add_s(array)
+   array.collect do |i|
+        if array[1] == i
+         i
+        else
+         i + 's'
+        end
+      end
+ end
