173579795899dfa4daee4dc72159146beafdf101
Richard Steinbach <steinbach.dick@gmail.com>
2017-07-05 22:28:13 -0700

    collections practice

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,81 @@
+def sort_array_asc (array)
 
+	array.sort do |a , b|
+		a <=> b
+	end
+end
+
+def sort_array_desc (array)
+	array.sort do |a, b|
+		b <=> a
+	end
+end
+
+def sort_array_char_count (array)
+	array.sort do |a, b|
+		a.length <=> b.length
+	end
+end
+
+def swap_elements (array)
+	temp = array[1]
+	array[1] = array[2]
+	array[2] = temp
+	return array
+	end
+def swap_elements_from_to (array, i1, i2)
+	temp = array[i1]
+	array[i1] = array[i2]
+	array[i2] = temp
+	return array
+end
+
+def reverse_array (array)
+array.reverse
+end
+
+def kesha_maker (array)
+	kesha_array = array.each do |a|
+	a[2] = "$"
+	end	
+end
+
+def find_a (array)
+	array.find_all do |a|
+		a.start_with?("a") 
+	end
+	
+end
+
+#def sum_array(array)
+#	sum = 0
+#	array.each do |a|
+#		sum = sum + a
+#	end
+#	sum
+#end
+
+def sum_array(array)
+	array.inject do |sum, n| sum + n
+	end
+end
+
+
+
+def add_s(array)
+	new_arr = []
+	new_arr = array.each_with_index do |a, i|
+		if i != 1
+			a << "s"
+	end
+	end
+	new_arr
+end
+
+#def add_s(array)
+#array.each_with_index.collect do |a, i|
+	
+#		a << "s" if i != 1
+	
+#	end
+#end
--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,18 @@ describe 'collections practice' do
     end
   end
 
+  describe '#swap_elements_from_to' do
+    it 'swap the  and third elements of an array' do
+      expect(swap_elements_from_to(["blake", "ashley", "scott"], 1, 2)).to eq(["blake", "scott", "ashley"])
+    end
+  end
+
+  describe '#swap_elements_from_to' do
+    it 'swap the first and second elements of an array' do
+      expect(swap_elements_from_to(["blake", "ashley", "scott"], 0, 1)).to eq(["ashley", "blake", "scott"])
+    end
+  end
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array' do
