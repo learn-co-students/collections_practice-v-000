34557ea121c968e5de09ba333bc91986d70685f6
Joanna Sese <jmgsese@gmail.com>
2017-07-19 22:35:54 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
+def sort_array_asc(array)
+  array.sort {|a, b| a<=>b}
+end
 
+def sort_array_desc(array)
+  array.sort {|a, b| b<=>a}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.size<=>b.size}
+end
+
+def swap_elements(array)
+  array.sort {|a,b| a[1]<=>b[2]}
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.map do |name|
+    name.sub!(name[2], "$")
+  end
+end
+
+def find_a(array)
+  array.select {|word| word.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject {|sum,n| sum + n}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index != 1
+      element << "s"
+    else
+      element
+    end
+  end
+end
