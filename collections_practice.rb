2755c5a78e79e98dcc6af73d209f6f633771956b
Mariel F <marielbeth@gmail.com>
2017-07-24 16:15:27 +0000

    all tests passing, including new one for swap_elements_from_to

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def swap_elements_from_to(array, start_index, destination_index)
+  array[start_index], array[destination_index] = array[destination_index], array[start_index]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  keshaed = []
+  array.each do |word|
+    split_wd = word.split("")
+    split_wd[2] = "$"
+    keshaed << split_wd.join
+  end
+  keshaed
+end
+
+def find_a(array)
+  array.select do |string|
+    string.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.reduce {|sum, num| sum + num}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |word, idx|
+     idx == 1 ? word : word << "s"
+  end
+end
