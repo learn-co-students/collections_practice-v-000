7f8e9786a27071ade4f3cd5fdcac2f6a2dafb909
Cody Kasselman <ckasselman@fordham.edu>
2017-08-01 18:22:28 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |b, a|
+    a <=> b
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.collect {|name| name[2] = "$"}
+array
+end
+
+def find_a(array)
+  array.select {|string| string[0] == "a"}
+end
+
+def sum_array(array)
+  array.inject {|sum,x| sum + x}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+  index != 1 ? element << "s" : element
+end
+end
