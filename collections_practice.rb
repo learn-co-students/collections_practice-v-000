b6ba832f9f34a5468b43aad4ad7fcce32a1ba295
Jennifer Nelson <jennifernelson118@gmail.com>
2017-08-09 22:34:29 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,61 @@
+def sort_array_asc(integers)
+  integers.sort
+end
 
+def sort_array_desc(integers)
+  integers.sort do |a,b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
+end
+
+# array = ["paul", "timothy", "bob", "sam", "jebediah"]
+
+def swap_elements(array)
+  array.insert(1,array.delete_at(2))
+end
+
+def reverse_array(integers)
+  integers.reverse!
+end
+
+def kesha_maker(array)
+  array.each {|name| name.gsub!(name[2], "$")}
+end
+
+def find_a(array)
+  array.select {|name| name.start_with?("a")}
+end
+
+def sum_array(array)
+  sum = 0
+  array.each {|a| sum+=a}
+  sum
+end
+
+def add_s(array)
+  array.each_with_index.collect do |word, index|
+    if index != 1
+    word << "s"
+  elsif index = 1
+    word
+    end
+  end
+end
