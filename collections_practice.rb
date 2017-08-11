122e10111ed278070f41c5a3b8ee7ea5b3f1e5e2
Matthew Nelson <nelson.matthew2015@gmail.com>
2017-08-11 02:47:55 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
+def sort_array_asc(array)
+  array.sort do |a,b|
+    a <=> b
+  end
+end
+def sort_array_desc(array)
+  array.sort do |b,a|
+    a <=> b
+  end
+end
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.size <=> b.size
+  end
+end
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+def reverse_array(array)
+  array.reverse
+end
+def kesha_maker(array)
+  array.each do |element|
+    element[2] = "$"
+    array
+end
+end
+def find_a(array)
+  array.select do |word|
+  word[0]=="a"
+end
+end
 
+def sum_array(array)
+  array.inject(0) do |sum, x|
+    sum + x
+  end
+end
+
+def add_s(array)
+  array.each_with_index.collect do |word, index|
+    if index == 1
+      word
+    else
+      word + "s"
+    end
+  end
+end
