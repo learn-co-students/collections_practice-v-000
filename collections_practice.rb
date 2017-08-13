259640d3f5990e7f13670a4922fd3d4148f31603
Mary Benitez <meebenitez@gmail.com>
2017-08-13 23:56:26 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort_by{ |i| -i }
+end
+
+def sort_array_char_count(array)
+  array.sort { |a, b| a.length <=> b.length }
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+  return array
+end
+
+def reverse_array(array)
+  array.reverse!
+end
+
+def kesha_maker(array)
+  array.each do |word|
+    word.sub!(word[2], "$")
+  end
+end
+
+def find_a(array)
+  array.select { |word| word.start_with? "a"}
+end
+
+def sum_array(array)
+  array.inject { |sum, n| sum + n}
+end
+
+def add_s(array)
+  array.each_with_index.collect { |element, index| index != 1 ? array[index] += "s" : array[index] = element }
+  return array
+end
