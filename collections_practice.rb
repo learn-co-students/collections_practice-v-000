9806168e00132ec6ab398aaac6e958bbe30ea13f
ronald vilorio <soccer23415@gmail.com>
2017-08-17 18:22:40 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+   array.sort {|x,y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+  first_e = array[1]
+  second_e = array[2]
+  array[1] = second_e
+  array[2] = first_e
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |char|
+    char[2] = "$"
+  end
+end
+
+def find_a(array)
+   array.select {|x| x.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject {|sum,n| sum + n}
+end
+
+def add_s(array)
+  array.each_with_index.collect {|element,index|
+    if index == 1
+      element
+    else
+      element << "s"
+    end
+  }
+end
