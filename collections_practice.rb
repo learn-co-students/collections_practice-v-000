a8038eed3ade721365b73a54010639c61d932acd
Yomaira Escano <yjescano@gmail.com>
2017-08-30 20:07:44 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
+def sort_array_asc(integers)
+  integers.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(integers)
+  integers.sort.reverse do |a, b|
+    a <=> b
+  end
+end
+
+def sort_array_char_count(strings)
+  strings.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array.sort do |a, b|
+    a[1] <=> b[2]
+  end
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |word|
+    word[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.find_all do |word|
+    word[0] == "a"
+  end
+end
+
+def sum_array(array)
+  array.inject do |sum, int|
+    sum += int
+  end
+end
+
+def add_s(array)
+  array.collect do |element|
+    if array[1] == element
+      element
+    else
+      element + "s"
+    end
+  end
+end
