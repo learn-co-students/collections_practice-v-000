49194a380f1affb753825bb792a526d7a83d518c
Christian Egglin <cegglin@conncoll.edu>
2017-09-01 17:56:33 +0000

    Done.

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,13 @@ describe 'collections practice' do
     end
   end
 
+  # Question 4.5
+  describe '#swap_elements_from_to' do
+    it 'swap the element at the first given index and element of the second given index of an array' do
+      expect(swap_elements_from_to(["a", "b", "c"],0,2)).to eq(["c", "b", "a"])
+    end
+  end
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array' do
