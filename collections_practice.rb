eca1d4eb552b6f01222544907607d899f7a26120
Christine <christinetran825@gmail.com>
2017-09-02 07:31:52 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,7 +5,7 @@ def sort_array_asc(array)
 end
 
 def sort_array_desc(array)
-  array.sort! { |a, b| b <=> a } #.sort!
+  array.sort { |a, b| b <=> a } #.sort!
 end
 
 def sort_array_char_count(array)
