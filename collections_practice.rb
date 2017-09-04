fa8adb051c9bc06c736b0cff6db52e21465392bd
Jason Kwong <jasonkwong11@gmail.com>
2017-09-03 20:36:30 -0700

    refactor

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,72 +1,43 @@
-def sort_array_asc(array)
-array.sort
+def sort_array_asc(arr)
+  arr.sort
 end
 
-def sort_array_desc(array)
-  array.sort! do |a, b|
-    if a == b
-      0
-    elsif a < b
-      1
-    elsif a > b
-      -1
-    end
+def sort_array_desc(arr)
+  arr.sort.reverse
+end
+
+def sort_array_char_count(arr)
+  arr.sort_by{|word| word.length}
+end
+
+def swap_elements(arr)
+  second = arr[1]
+  arr[1] = arr[2]
+  arr[2] = second
+  arr
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr)
+  arr.map do |word|
+    letter = word[2]
+    word.tr(letter, "$")
   end
 end
 
-def sort_array_char_count(array)
-array.sort
+def find_a(arr)
+  arr.select{|word| word[0] == "a"}
 end
 
-def swap_elements(array)
-  array[1], array[2] = array[0], array[2], array[1]
+def sum_array(arr)
+  arr.reduce(0){|b, x| b+x}
 end
 
-def reverse_array(array)
-array.reverse!
+def add_s(arr)
+  arr.each_with_index.collect{ |e, i| i == 1 ? e : e << "s" }
 end
-
-def kesha_maker(array)
-  array.each do |x|
-    x.gsub!(x[2],"$")
-  end
-end
-
-def find_a(array)
-collection = []
-array.each do |x|
-  if x.start_with?("a")
-    collection.push(x)
-    end
-  end
-  collection
-end
-
-def sum_array(array)
-  collection = 0
-  array.each do |x|
-    collection += x
-  end
-  collection
-end
-
-
-def add_s(array)
-  array.each_with_index.collect do |element, index|
-    if index != 1
-      element << "s"
-    end
-  end
-  array[1] = array[1]
-  array
-end
-
-
-
-
-
-
-
-
 
 
