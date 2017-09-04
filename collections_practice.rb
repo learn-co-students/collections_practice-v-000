e7c120a40b337530cf48c22cf59967d75d34605f
joey michalina mariano <joey.michalina.mariano@gmail.com>
2017-09-04 22:08:12 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,63 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array.sort do |a, b|
+    if array[1] == a
+      1
+    else
+      0
+    end
+  end
+end
+
+def reverse_array(array)
+  array.sort do |a, b|
+    1
+  end
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |a|
+    a.insert(2, "$")
+    a[3] = ""
+    new_array << a
+  end
+  new_array
+end
+
+def find_a(array)
+  array.delete_if do |a|
+    a[0] != "a"
+  end
+end
+
+def sum_array(array)
+  array.inject do |sum, n|
+    sum + n
+  end
+end
+
+def add_s(array)
+  array.collect do |a|
+    if a != array[1]
+      a + "s"
+    else
+      a
+    end
+  end
+end
