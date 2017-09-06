79266759506a5a6fec0275cf29f2ddb6278263ac
Justin Tooley <jstooley823@gmail.com>
2017-09-06 09:55:26 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,70 @@
+def sort_array_asc(array)
+  array.sort!
+end
 
+def sort_array_desc(array)
+  sort_array_asc(array).reverse!
+end
+
+def sort_array_char_count(array)
+  array.sort do |wordone,wordtwo|
+    wordone.length <=> wordtwo.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+  array
+end
+
+def reverse_array(array)
+  array.reverse!
+end
+
+def kesha_maker(array)
+  x = 0
+  new_array = []
+  array.each do |name|
+    temp_array = []
+    i = 0
+    name.each_char do|c|
+      temp_array[i] = c
+      i += 1
+    end
+    temp_array[2] = "$"
+    new_array[x] = temp_array.join
+    x += 1
+  end
+  new_array
+end
+
+def find_a(array)
+  a_array = []
+  i = 0
+  array.each do |word|
+     if word.start_with?("a")
+       a_array[i] = word
+       i += 1
+     end
+  end
+  a_array
+end
+
+def sum_array(array)
+  array.inject {|sum, n| sum+n }
+end
+
+def add_s(array)
+  array.each_with_index.collect do |word,i|
+    if i == 1
+      word
+    else
+      word << "s"
+    end
+  end
+end
--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,13 @@ describe 'collections practice' do
     end
   end
 
+  # Question 4.5
+  describe '#swap_elements_from_to' do
+    it 'swap the index and destination_index elements of an array' do
+      expect(swap_elements_from_to(["blake", "ashley", "scott"],0,2)).to eq(["scott", "ashley", "blake"])
+    end
+  end
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array' do
