2632a1db4932d7d84dae3b523a32e6cd1f86cdc5
Cristina Collazos <clcgalex@gmail.com>
2017-09-15 00:21:22 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,46 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array = array.sort
+  array.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array.sort do |a,b|
+    a[1] <=> b[2]
+  end
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each {|artist| artist[2] = "$"}
+end
+
+def find_a(array)
+  array.select {|word| word.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject {|a,b| a + b}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |word, i|
+  if i == 1
+    word
+  else
+    word + "s"
+  end
+  end
+end
