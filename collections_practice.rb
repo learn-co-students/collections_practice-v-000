f5314dbb99f45c6c2819f1c9b90355a4e0a3a69f
Kristen Peck <kpeck71@gmail.com>
2017-09-18 22:13:03 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array.each_with_index do |index|
+    array[1],array[2] = array[2],array[1]
+  end
+end
+
+def reverse_array(array)
+  array.reverse!
+end
+
+def kesha_maker(array)
+  array.each_with_index do |string,index|
+    string[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.find_all {|string| string.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject {|sum,n| sum + n}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |i,index|
+    if index != 1
+      i + "s"
+    else
+      i
+    end
+  end
+
+end
