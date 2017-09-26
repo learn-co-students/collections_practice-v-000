06bd1e77f6c7241ddf77e2d7d60672493308b11f
camneu37 <neuner.camille@gmail.com>
2017-09-25 20:47:55 -0700

    Done.

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,13 @@ describe 'collections practice' do
     end
   end
 
+  #Advanced Question 4
+  describe '#swap_elements_from_to' do
+    it 'swap element from one specified index to another specified index of the array' do
+      expect(swap_elements_from_to(["a", "b", "c"],0,2)).to eq(["c", "b", "a"])
+    end
+  end
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array of integers' do
