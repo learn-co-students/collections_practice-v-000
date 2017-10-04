e8b6bd752bf61a20a082aea39247fc7f5f47fce8
Michael Hirschberger <mlhirschberger@gmail.com>
2017-10-04 03:49:42 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,64 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.size <=> b.size
+  end
+end
+
+def swap_elements(array)
+  new_array = []
+  array.each_with_index do |element, index|
+    if index == 1
+      new_array << array[2]
+    elsif index == 2
+      new_array << array[1]
+    else
+      new_array << element
+    end
+  end
+  new_array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |element|
+    element[2] = "$"
+    new_array << element
+  end
+  new_array
+end
+
+def find_a(array)
+  array.select { |element| element[0] == "a" }
+end
+
+def sum_array(array)
+  array.inject { |sum, n| sum + n }
+end
+
+def add_s(array)
+  new_array = []
+  array.each_with_index do |element, index|
+    if index != 1
+      new_array << "#{element}s"
+    else
+      new_array << element
+    end
+  end
+  new_array
+end
