fc05fdf8edc77a8af00c5ce5bb196aae3138629c
Trisha Garrett <trisha.garrett@gmail.com>
2017-10-12 23:52:37 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,23 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+
+
+  #count = 0
+  array.each {|s| count += s.length}
+end
