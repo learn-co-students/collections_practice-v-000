01e917f6426474c1de87dcfed6952327a1fa5011
Adam Clayman <adam@clayman.org>
2017-10-17 02:13:25 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,62 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.length < b.length
+      -1
+    elsif a.length == b.length
+      0
+    elsif a.length > b.length
+      1
+    end
+  end
+end
+
+def swap_elements(array)
+  temp = array[1]
+  array[1] = array[2]
+  array[2] = temp
+  return array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  kesha_array = []
+  array.each do |string|
+    string[2] = "$"
+    kesha_array << string
+  end
+  return kesha_array
+end
+
+def find_a(array)
+  array.select do |string|
+    string.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  sum = 0
+  array.map do |a|
+    sum += a
+  end
+  return sum
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index != 1
+      array[index] = element + "s"
+    end
+  end
+  return array
+end
