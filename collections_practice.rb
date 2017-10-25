f47d11af5cc7a13d621f7a5104fab6374058c089
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-25 18:18:19 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,85 @@
+def sort_array_asc(arr)
+  return arr.sort
+end
 
+def sort_array_desc(arr)
+  return arr.sort{|a,b|
+    if b>a
+      1
+    elsif b<a
+      -1
+    else
+      0
+    end
+  }
+end
+
+def sort_array_char_count(arr)
+  return arr.sort{|a,b|
+    if a.size > b.size
+      1
+    elsif a.size < b.size
+      -1
+    else
+      0
+    end
+  }
+end
+
+def swap_elements(arr)
+  swap = arr[2]
+  arr[2] = arr[1]
+  arr[1] = swap
+  arr
+end
+
+def swap_elements_from_to(arr,a,b)
+  swap = arr[b]
+  arr[b] = arr[a]
+  arr[a] = swap
+  arr
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr)
+  coll = []
+  arr.each{|e|
+    strArr = e.split('')
+    strArr[2] = "$"
+    coll << strArr.join('')
+  }
+  return coll
+end
+
+def find_a(arr)
+  arr.select{|e|
+    e.start_with?("a")
+  }
+end
+
+def sum_array(arr)
+  sum = 0
+  arr.each{|e|
+    sum += e
+  }
+  sum
+end
+
+def sum_array_inject(arr)
+  arr.inject{|sum,e|
+    sum + e
+  }
+end
+
+def add_s(arr)
+  arr.each_with_index.collect{|val,index|
+    if index != 1
+      val + "s"
+    else
+      val
+    end
+  }
+end
--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -24,6 +24,13 @@ describe 'collections practice' do
   end
 
   # Question 4
+  describe '#swap_elements_from_to' do
+    it 'swap the second and third elements of an array' do
+      expect(swap_elements_from_to(["blake", "ashley", "scott"],0,2)).to eq(["scott", "ashley", "blake"])
+    end
+  end
+
+  # Question 4
   describe '#swap_elements' do
     it 'swap the second and third elements of an array' do
       expect(swap_elements(["blake", "ashley", "scott"])).to eq(["blake", "scott", "ashley"])
