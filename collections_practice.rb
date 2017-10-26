2f7b04e3ca9cef00a515dce6c9541cd59b6870ea
Joshua Needham <joshuabneedham@gmail.com>
2017-10-26 00:43:59 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+require 'pry'
+def sort_array_asc(array)
+  array.sort
+end
+def sort_array_desc(array)
+  array.sort { |a, b| b <=> a}
+end
 
+def sort_array_char_count(array)
+  array.sort_by {|word| word.length }
+end
+
+def swap_elements(array)
+  array = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  copy = array.reverse
+  copy
+
+end
+
+def kesha_maker(array)
+  array.each do |name|
+    name[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select { |word| word.start_with?("a") }
+end
+
+def sum_array(array)
+  array.inject(0) {|sum, x| sum + x }
+end
+
+def add_s(array)
+  new_array = []
+  array.each do |part|
+    if part == "feet"
+      new_array << "#{part}"
+    else
+      new_array << "#{part}s"
+    end
+  end
+  new_array
+end
