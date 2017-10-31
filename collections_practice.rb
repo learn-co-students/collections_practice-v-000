5428241b72577e9afcaa2ac4c49f1a258f48b70b
Chad Montoya <chadmontoya21@yahoo.com>
2017-10-31 19:32:50 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+
+def sort_array_desc(array)
+  array.sort {|a, b| b <=> a}
+end
+
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.length <=> b.length}
+end
+
+
+def swap_elements(array)
+  array.slice(1,2).sort{|a,b| b<=>a}.unshift(array[0])
+end
+
+
+
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  k = []
+  array.each {|n| k << n.sub!(n[2], "$") }
+  k
+end
+
+def find_a(array)
+  array.select {|i| i.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject(:+)
+  # array.inject {|sum, n| sum + n}
+end
+
+def add_s(array)
+f = array.slice!(1, 1).join
+array.map {|i| i + "s"}.insert(1, f)
+end
