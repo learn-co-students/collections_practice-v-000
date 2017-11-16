2e9199af137a1dfec59ca657fe7b597bc932270c
Tyler Pulse <tyler3321@gmail.com>
2017-11-16 04:22:54 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|x| x.length}
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+array.map {|dollar| dollar.gsub(dollar[2], "$")}
+end
+
+def find_a(array)
+  array.select {|first| first.start_with?("a")}
+end
+
+def sum_array(array)
+  array.reduce {|sum, n| sum + n}
+end
+
+
+
+def add_s(array)
+array[0] = array[0] + ("s"), array[1] = array[1], array[2] = array[2] + ("s"), array[3] = array[3] + ("s")
+end
