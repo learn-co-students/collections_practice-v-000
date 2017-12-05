9a2c38fc22b6de8d07c5524d1c0cf4a7d58a2e52
James Ferguson <ncjamesferguson@gmail.com>
2017-12-05 15:56:40 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,62 @@
+def sort_array_asc(arr)
+  arr.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(arr)
+  arr.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(arr)
+  arr.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(arr)
+  x = arr[2]
+  arr[2] = arr[1]
+  arr[1] = x
+
+  arr
+
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr)
+  arr.each do |word|
+    word[2] = "$"
+  end
+end
+
+def find_a(arr)
+  arr.find_all do |word|
+    word.start_with?("a")
+  end
+end
+
+def sum_array(arr)
+  sum = 0
+  arr.each do |num|
+    sum += num
+  end
+  sum
+end
+
+def add_s(arr)
+  a = []
+     arr.each_with_index do |word, index|
+      if index != 1
+      a[index] = word += "s"
+      else
+      a[index] = word
+    end
+  end
+  a
+end
