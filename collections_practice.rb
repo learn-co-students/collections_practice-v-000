66700eaad30c8f9ca9b0c6688c52082c2d940c78
Eric Johnson <0216johnsoneric@gmail.com>
2017-12-09 17:52:34 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
+def roll_call_dwarves# code a parameter here
+  # Your code here
+end
 
+def summon_captain_planet# code a parameter here
+  # Your code here
+end
+
+def long_planeteer_calls# code a parameter here
+  # Your code here
+end
+
+def find_the_cheese# code a parameter here
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+end
