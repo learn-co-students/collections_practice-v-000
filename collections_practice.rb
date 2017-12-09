ce532ba386b5e420542336fd773c818a34175009
Eric Johnson <0216johnsoneric@gmail.com>
2017-12-09 17:53:19 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,16 +1,72 @@
-def roll_call_dwarves# code a parameter here
-  # Your code here
+require 'pry'
+
+def sort_array_asc(array)
+  array.sort
 end
 
-def summon_captain_planet# code a parameter here
-  # Your code here
+def sort_array_desc(array)
+  array.sort do | left, right|
+    right <=> left
+  end
 end
 
-def long_planeteer_calls# code a parameter here
-  # Your code here
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
 end
 
-def find_the_cheese# code a parameter here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.find_all do |word|
+    word[0] == "a"
+  end
+
+  # using select method
+    # array.select do |word|
+    #   word[0] == "a"
+    # end
+end
+
+def sum_array(array)
+  sum = 0
+  array.each do |num|
+    sum+=num
+  end
+  sum
+
+  # using reduce method
+    # array.reduce(:+)
+
+  # using inject method (short)
+     # array.inject(:+)
+
+  # using inject method (long)
+     # array.inject do |sum,x|
+     #  sum + x
+     # end
+end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
 end
