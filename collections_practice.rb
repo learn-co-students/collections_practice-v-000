b97642da783bc1eb08a50942af0855b305e112ce
jtorre20 <jtorres1989@gmail.com>
2017-12-12 01:56:09 -0500

    initial commit

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,23 @@
+def sort_array_asc(array)
+    array.sort
+end
 
+def sort_array_desc(array)
+    array.sort {|x, y| y <=>x}
+end
+
+def sort_array_char_count(array)
+    array.sort {|x, y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+    array[0],array[1], array[2] = array[0],array[2], array[1]
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+
+end
