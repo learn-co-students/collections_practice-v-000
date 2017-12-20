a2b4f41c46acc1d17c5a01879d887d27cb70f797
Aubrey Neimeier <neimeier.1@osu.edu>
2017-12-20 19:28:25 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -30,18 +30,63 @@ def reverse_array(array)
   array.reverse!
 end
 
+
 def kesha_maker(array)
   new_array = []
+  new_word_array = []
+  new_word = ""
   array.each do |element|
-    new_array << array.insert(2, "$")
+    new_word_array = element.chars.to_a
+    new_word_array[2].replace("$")
+    new_word = new_word_array.join
+    new_array << new_word
   end
   new_array
 end
 
 
-=begin
 def swap_elements(array)
   new_array = array.insert(1, array[2])
-  new_array.delete(array[3])
+  new_array.pop
+  new_array
+end
+
+
+def find_a(array)
+  a_words = []
+  array.select do |word|
+    if word.start_with?("a")
+      a_words << word
+    end
+  end
+end
+
+def sum_array(array)
+  total = 0
+  array.each do |num|
+    total += num
+  end
+  total
+end
+
+def add_s(array)
+  new_words = []
+  array.each do |word|
+    if word == "feet"
+      new_words << word
+    else
+        new_words << word + "s"
+    end
+  end
+  new_words
+end
+
+=begin my 1st attempt at kesha_maker. broken
+def kesha_maker(array)
+  new_array = []
+  array.each do |element|
+    new_array << element.insert(2, "$").delete(3)
+  end
+  new_array
 end
 =end
