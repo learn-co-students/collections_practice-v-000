5d84d9aa672b18559c9252056711910feff56cc7
David Towster <djt4xe@virginia.edu>
2017-12-20 04:07:25 -0600

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
+def sort_array_asc(integers)
+    integers.sort do |a, b|
+        a <=> b
+    end
+end
 
+def sort_array_desc(integers)
+    integers.sort do |a, b|
+        b <=> a
+    end
+end
+
+def sort_array_char_count(strings)
+    strings.sort do |a, b|
+        a.length <=> b.length
+    end
+end
+
+def swap_elements(array)
+    a = array[1]
+    b = array[2]
+    array[1] = b
+    array[2] = a
+    array
+end
+
+def reverse_array(integers)
+    integers.reverse
+end
+
+def kesha_maker(strings)
+    strings.each do |string|
+        string[2] = "$"
+    end
+end
+
+def find_a(array)
+    array.select do |i|
+        i.start_with?("a")
+    end
+end
+
+def sum_array(integers)
+    integers.inject do |sum, n|
+        sum + n
+    end
+end
+
+def add_s(array)
+    array.each_with_index do |word, i|
+        if i != 1
+        word << "s"
+        end
+    end
+end
