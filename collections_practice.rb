17f8032df709960801a9c7f104c83dd0b999ea3d
Aubrey Neimeier <neimeier.1@osu.edu>
2017-12-20 16:51:29 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+def sort_array_asc(integers)
+  integers.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a > b
+      -1
+    elsif a < b
+      1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
+end
+
+def reverse_array(array)
+  array.reverse!
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |element|
+    new_array << array.insert(2, "$")
+  end
+  new_array
+end
+
+
+=begin
+def swap_elements(array)
+  new_array = array.insert(1, array[2])
+  new_array.delete(array[3])
+end
+=end
