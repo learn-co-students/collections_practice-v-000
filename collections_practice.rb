4b4883ccf3a7f3fddeac74e81a5e6327fc2339e9
DanL12186 <danlrcn@hotmail.com>
2017-12-26 17:39:55 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,57 +1,37 @@
 def sort_array_asc(array)
-array.sort()
+  array.sort
 end
 
 def sort_array_desc(array)
-array.sort.reverse()
+  array.sort { |a, b| b - a }
 end
 
 def sort_array_char_count(array)
-array.sort do |a,b|
-a.length <=> b.length
-end
+  array.sort_by { |str| str.length }
 end
 
 def swap_elements(array)
-  array.insert(3,array[1])
+  array.insert(3, array[1])
   array.delete_at(1)
   return array
 end
-
 
 def reverse_array(array)
   array.reverse
 end
 
 def kesha_maker(array)
-  for i in array
-  i.sub!(i[2],"$")
-end
+  array.map { |str| str.sub(str[2], '$') }
 end
 
 def find_a(array)
-  container = []
-  array.each do |string|
-    if string.start_with?("a")
-      container.push(string)
-    end
-  end
-  return container
+  array.select { |string| string.start_with?("a") }
 end
 
 def sum_array(array)
-  total = 0
-  array.each do |int|
-    total += int
-  end
-  return total
+  array.reduce(:+)
 end
 
 def add_s(array)
-  final = []
-  for i in array
-    final.push(i + 's')
-  end
-  final[1] = array[1]     #kind of a cheap hack, but I'm pretty tired >_>
-  return final
+  array.map.with_index { |str, i| i == 1 ? str : str << 's' }
 end
