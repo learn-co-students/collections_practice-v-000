29d152455064927b48af75f67a40f9ab655e52fe
neodynia <neodynia@icloud.com>
2018-01-01 01:38:34 -0500

    DONE

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,18 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|x,y| y <=> x}
+end
+
+def sort_array_char_count(array)
+  array.sort {|x,y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+end
+
+def reverse_array(array)
+  array.reverse
+end 
