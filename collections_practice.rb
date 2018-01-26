96522f3cd45feb9df05d86c901b439459d1a3d45
Katrina Brinson <kbrinso@gmail.com>
2018-01-26 23:50:45 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(array)
+  new_array = array.sort
+end
 
+def sort_array_desc(array)
+  new_array = array.sort
+  new_array = new_array.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort{|a,b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2],array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = array
+  new_array.each{|x| x[2] = "$"}
+end
+
+def find_a(array)
+  array.select{|x| x[0] == "a"}
+end
+
+def sum_array(array)
+  array.reduce(:+)
+end
+
+def add_s(array)
+  array.each_with_index.collect{|element, index| 
+  if index != 1
+    element << "s"
+  else
+    element
+  end
+  }
+end
