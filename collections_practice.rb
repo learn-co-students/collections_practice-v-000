40a53f21d457ba40c9acbab606c8828fd05a4fc6
Eden Mesfun <emesfun@gmail.com>
2018-01-28 23:06:28 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
+require 'pry'
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by { |a| a.length }
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each_with_index { |element, index| element[2] = "$"}
+end #method
+
+def find_a(array)
+  array.select { |word| word.start_with?('a')}
+end
+
+def sum_array(array)
+  array.inject(:+)
+end
+
+def add_s(array)
+  new_array = array.each_with_index.collect { |element, index| index != 1 ? element + "s" : element }
+  new_array
+end #method
