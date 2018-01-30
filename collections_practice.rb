555e5358f71d611c238aec45c5f546e6bb22f254
Rollin Metzger <rollin.metzger@gmail.com>
2018-01-30 17:35:36 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array.sort {|a, b| a[1] <=> b[2]}
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(strings)
+  strings.each {|word| word[2] = "$"}
+end
+
+def find_a(array)
+  array.select {|word| word.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject {|sum, n| sum + n}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |ele, index|
+    if index == 1
+      ele
+    else
+      ele + "s"
+    end
+  end
+end 
