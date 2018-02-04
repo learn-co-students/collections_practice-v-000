489d59b9ef0dfc8ba6362d176520fe7dad1a75f0
Stephen Harrington <stephenharrington04@yahoo.com>
2018-02-04 22:39:37 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,87 +1,51 @@
-
-def sort_array_asc(numbers)
-  numbers.sort
+def sort_array_asc(array)
+  array.sort
 end
 
-def sort_array_desc(numbers)
-  numbers.sort do |a, b|
-    if a == b
-      0
-    elsif a < b
-      1
-    elsif a > b
-      -1
-    end
-  end
+def sort_array_desc(array)
+  array.sort {|a, b| b <=> a}
 end
 
-def sort_array_char_count(strings)
-  strings.sort do |a, b|
-    if a.length == b.length
-      0
-    elsif a.length < b.length
-      -1
-    elsif a.length > b.length
-      1
-    end
-  end
+def sort_array_char_count(array)
+  array.sort {|a, b| a.size <=> b.size}
 end
 
 def swap_elements(array)
-  a = array[1]
-  b = array[2]
-  array[1] = b
-  array[2] = a
+  array[1], array[2] = array[2], array[1]
   array
 end
 
 def swap_elements_from_to(array, index, destination_index)
-  a = array[index]
-  b = array[destination_index]
-  array[index] = b
-  array[destination_index] = a
+  array[index], array[destination_index] = array[destination_index], array[index]
   array
 end
 
 def reverse_array(array)
-  new_array = []
-  array.each do |number|
-    new_array.unshift(number)
-  end
-  new_array
+  array.reverse
 end
 
 def kesha_maker(array)
-  new_array = []
   array.each do |string|
     string[2] = "$"
-    new_array << string
   end
-  new_array
 end
 
 def find_a(array)
-  array.select do |string|
+  array.find_all do |string|
     string.start_with?("a")
   end
 end
 
 def sum_array(array)
-  #sum = 0
-  #array.each do |number|
-  #  sum += number
-  #end
-  #sum
-  array.inject do |sum, n|
-    sum + n
-  end
+  sum = 0
+  array.each {|number| sum += number}
+  sum
 end
 
 def add_s(array)
-  array.each_with_index.collect do |string, index|
+  array.each_with_index do |string, index|
     if index != 1
       string << "s"
-    else string
     end
   end
 end
