018f197ce2dc936630071fe1e33920567e06f472
sgibson47 <sgibson47@gmail.com>
2018-02-08 17:04:23 -0600

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|a,b| b<=>a}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a,b| a.length<=>b.length}
+end
+
+def swap_elements(array)
+  array[1],array[2]=array[2],array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array_2=[]
+  array.each do |string|
+    string[2,1]="$"
+    array_2<<string
+  end
+  array_2
+end
+
+def find_a(array)
+  array_2=[]
+  array.each do |string|
+    if string.start_with?("a")
+      array_2<<string
+    end
+  end
+  array_2
+end
+
+def sum_array(array)
+  array.inject {|sum, n| sum + n }
+end
+
+def add_s(array)
+  array_2=[]
+  array.each_with_index do |element, index|
+    if index==1
+      array_2<<element
+    else
+      array_2<<"#{element}s"
+    end
+  end
+  array_2
+end
