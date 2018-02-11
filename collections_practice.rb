b824bc9269a1e09c36e5521bd07a7a2ffe612f2f
Trac Nguyen <nguyendtrac@gmail.com>
2018-02-11 22:35:08 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+  b <=> a
+end
+end
+
+def sort_array_char_count(array)
+  array.sort { |a, b| [a.size, a] <=> [b.size, b] }
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  kesha = []
+  array.each do |word|
+    word[2] = "$"
+    kesha << word
+  end
+  kesha
+end
+
+def find_a(array)
+  array.select{|element| element.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject { |n, sum| sum + n}
+end
+
+def add_s(array)
+ array.each_with_index.collect{|element, index| index != 1 ? element + "s" : element}
+end
