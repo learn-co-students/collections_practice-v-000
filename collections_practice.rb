0c3d9f8b5d56ffce50f2d15e38eec01fbe80a67d
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-02-20 19:34:11 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,57 @@
+#sort in ascending order
+def sort_array_asc(array)
+  array.sort {|a,b| a <=> b}
+end
 
+#sort in descending order
+def sort_array_desc(array)
+  array.sort {|a,b| b <=> a}
+end
+
+#sort by string length (ascending)
+def sort_array_char_count(array)
+  array.sort {|a,b| a.length <=> b.length}
+end
+
+#swaps the second and third elements
+def swap_elements(array)
+  array.insert(1, array.delete_at(2))
+end
+
+#return a reverse array
+def reverse_array(array)
+  new_array = [ ]
+  array.each do |word|
+    new_array.unshift(word)
+  end
+
+  return new_array
+end
+
+#turns third char into a dollar sign ('$')
+def kesha_maker(array)
+  array.each do |name|
+    name[2] = '$'
+  end
+end
+
+#returns all strings that start with the letter 'a'
+def find_a(array)
+  array.select{|name| name.start_with?('a')}
+end
+
+#returns the sum of all the numbers in the array
+def sum_array(array)
+  array.inject(0) {|result_memo, element| result_memo + element}
+end
+
+#adds an 's' to the end of each array element, except for the second element
+def add_s(array)
+  array.each_with_index.collect do |word, index|
+    if (index == 1)
+      word = word
+    else
+      word = word + 's'
+    end # if statement
+  end # collect method
+end # self
