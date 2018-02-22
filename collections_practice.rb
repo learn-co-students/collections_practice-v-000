43b0454f834a2916af47780a9d7f2268ae84915d
Katie Cassell <kcassell944@gmail.com>
2018-02-22 00:39:52 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,11 @@
+def sort_array_asc(array)
+ array.sort
+end
 
+def sort_array_desc(array)
+  array.sort { |x,y| y <=> x}
+end
+
+def sort_array_char_count(array)
+  array.sort { |element| element.length}
+end
