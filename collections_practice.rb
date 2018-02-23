38be44c32f6865dd54eedb57ac77831b181de0ea
Katie Cassell <kcassell944@gmail.com>
2018-02-23 00:33:08 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,5 +7,40 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-  array.sort { |x,y| }
+  array.sort { |x,y| x.length <=> y.length }
+end
+
+def swap_elements(array)
+   array[1], array[2] = array[2], array[1]
+   return array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  for i in 0...array.length
+    array [i][2] = "$"
+  end
+  array
+end
+
+def find_a(array)
+  array.select do |x|
+    x[0] == "a"
+  end
+end
+
+def sum_array(array)
+  array.inject(:+)
+end
+
+def add_s(array)
+  for i in 0...array.length
+   if i != 1
+      array[i] += "s"
+    end
+  end
+  array
 end
