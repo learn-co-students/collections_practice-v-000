a78f7453d0c4fcf3abd1897f2d360253bea16e6f
Christine Cua <cloudycua@gmail.com>
2018-02-26 22:24:18 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,13 +13,11 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-#  old_second = array[1]
-#  old_third = array[2]
-#  array[1] = old_third
-#  array[2] = old_second
-#  array
-array[1], array[2] - array[2], array[1]
-array
+  old_second = array[1]
+  old_third = array[2]
+  array[1] = old_third
+  array[2] = old_second
+  array
 end
 
 def reverse_array(array)
