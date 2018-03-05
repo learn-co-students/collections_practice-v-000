92af4dfb97b62802bdf21f1b753439afd1077e0f
Aspen James <aspenjames@tqca.org>
2018-03-05 02:50:09 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,9 +13,7 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  hold = array[2]
-  array[2] = array[1]
-  array[1] = hold
+  array[1], array[2] = array[2], array[1]
   array
 end
 
