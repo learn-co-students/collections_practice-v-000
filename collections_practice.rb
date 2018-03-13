d2f24433fcf4f90f0ef7c53e3d874fbf8f4ba4f3
Brett Weisberg <weisberg.brett@gmail.com>
2018-03-13 01:37:59 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
+def sort_array_asc(array)
+  array.sort do |a,b|
+    a<=>b
+    end
+  end
 
+  def sort_array_desc(array)
+    array.sort! do |a,b|
+      b<=>a
+  end
+end
+
+  def sort_array_char_count(array)
+    array.sort{|x, y| x.length <=> y.length}
+  end
+
+  def swap_elements(array)
+  end
+
+  def reverse_array(array)
+    array.reverse
+end
+
+  def kesha_maker(array)
+  end
+
+  def find_a(array)
+  end
+
+  def sum_array(array)
+    array.inject{|sum, n| sum + n }
+  end
+
+  def add_s(array)
+    [1,2].each_with_index.collect{|element, index| }
+  end
