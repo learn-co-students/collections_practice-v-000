dc98f6e48ba2a4416babd3de5fc3f681ecdc85e0
Emma Freeman <freemoe04@gmail.com>
2018-03-15 17:04:36 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,73 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif
+      a > b
+      -1
+    elsif
+      a < b
+      1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+array.sort do |a, b|
+  if a.length == b.length
+    0
+  elsif
+    a.length > b.length
+    1
+  elsif
+    a.length < b.length
+    -1
+  end
+  end
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+end
+
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+tick_tock = array.each do |word|
+    word[2] = "$"
+  end
+  tick_tock
+  end
+
+  def find_a(array)
+  array =  array.select do |element|
+      element.start_with?("a")
+      end
+  return array
+  end
+
+
+def sum_array(array)
+array.inject do |sum, n|
+  sum + n
+end
+end
+
+def add_s(array)
+  array.collect do |element|
+      if element == array[1]
+        element
+      else
+        element + "s"
+        end
+    end
+end
