2bca6221d44ad3a2dbab7d32c2fd66a06a0a25a1
Brett Weisberg <weisberg.brett@gmail.com>
2018-03-16 00:48:12 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,41 +3,41 @@ def sort_array_asc(array)
     a<=>b
     end
   end
-
+  
   def sort_array_desc(array)
-    array.sort! do |a,b|
-      b<=>a
-  end
+  array.sort! do |a,b|
+    b<=>a
+end
 end
 
   def sort_array_char_count(array)
-    array.sort{|x, y| x.length <=> y.length}
-  end
+      array.sort{|x, y| x.length <=> y.length}
+    end
 
-  def swap_elements(array)
-  end
+    def reverse_array(array)
+        array.reverse
+    end
 
-  def reverse_array(array)
-    array.reverse
-end
-
-  def kesha_maker(array)
-  end
-
-  def find_a(array)
-  end
-  
-
-  def sum_array(array)
-    array.inject{|sum, n| sum + n }
-  end
-
-  def add_s(array)
-    array.each do |words|
-    if words == array[1]
-      words
-    else
-        words << "s"
+    def kesha_maker(array)
+      array.each do |word|
+        word[2] = "$"
       end
     end
+
+    def find_a(array)
+        array.select { |word| word.start_with?('a') }
+      end
+
+      def sum_array(array)
+  array.inject{|sum, n| sum + n }
+end
+
+def add_s(array)
+  array.each do |words|
+  if words == array[1]
+    words
+  else
+      words << "s"
+    end
   end
+end
