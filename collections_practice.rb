36a5dc9c1279b803466c6c45b606cc3409c85429
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-24 18:58:17 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(array)
+  array.sort {|a, b| a <=> b}
+end
 
+def sort_array_desc(array)
+  array.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array(array)
+  array.reverse!
+  return array
+end
+
+def kesha_maker(array)
+  array.each {|item| item[2] = "$"}
+end
+
+def find_a(array)
+  array.find_all {|word| word[0] == "a"}
+end
+
+def sum_array(array)
+  sum = 0
+  array.each {|num| sum+=num}
+  sum
+end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end  
+end
