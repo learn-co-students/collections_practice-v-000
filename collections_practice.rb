8e456f0d9fda4660a40d86c72cf7b6ac6c822986
Brent Bauman <brentbauman86@gmail.com>
2018-04-20 00:28:16 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
+#require "pry"
+def sort_array_asc(integers)
+  integers.sort {|a, b| a <=> b}
+end
 
+def sort_array_desc(integers)
+  integers.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(element)
+  element.sort {|a, b| a.length <=> b.length}
+end
+
+def swap_elements(element)
+  element.sort {|a, b| a[1] <=> b[2]}
+end
+
+def reverse_array(element)
+  element.sort {|a, b| a <=> b}
+element.reverse
+end
+
+def kesha_maker(element)
+
+  element.collect do |cash|
+    cash[2] = "$"
+    cash
+end
+end
+
+def find_a(words)
+  words.select {|a| a.start_with?("a")}
+end
+
+
+def sum_array(total)
+  total.inject{|sum, n| sum + n}
+end
+
+def add_s(slaps)
+  slaps.each_with_index.collect{|s, index| index == 1 ? s : "#{s}s"}
+
+end
