3eda65469c169b672d2fd181c64583ccf13ead4c
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-24 16:34:20 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(array)
+  array.sort
+end 
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a 
+end 
+end 
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+    end 
+end 
+
+def swap_elements(array)
+array[1], array[2] = array[2], array[1]
+array 
+end 
+
+def reverse_array(array)
+  array.reverse 
+end 
+
+def kesha_maker(array)
+  array.each do |a|
+    a[2] = "$"
+  end 
+end 
+
+def find_a(array)
+  array.select {|string| string.start_with?("a")}
+end 
+
+def sum_array(array)
+  array.inject(0) { |result, element| result + element } 
+end 
+
+def add_s(array)
+  array.each_with_index.collect {|element, index| index == 1 ? element : element << "s"}
+end 
+
 
