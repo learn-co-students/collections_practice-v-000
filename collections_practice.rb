0ba2a0e3058a475ea71cb54134c40d902352349b
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-04-29 17:44:55 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+end
+end
+
+def sort_array_char_count(array)
+  array.sort { |a, b|
+    a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each { |char| char[2] = "$"}
+end
+
+def find_a(array)
+  array.select { |char| char.start_with? ("a")}
+end
+
+def sum_array(array)
+array.inject :+
+end
+
+def add_s(array)
+  array.each_with_index { |element, index| element << "s" if index != 1}
+end
