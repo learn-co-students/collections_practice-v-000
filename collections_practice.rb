ea23d56db5795c7f6b901d5b9adedaad6c5c9727
Daksha Patel <dakshapatel100@gmail.com>
2018-05-04 19:56:12 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,10 +22,28 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
-  counter = 1
-  new_array= []
-  array.each do {|dollar_sign| array [2]}
-    counter += 1
+
+  array.each do |dollar_sign|
+    dollar_sign[2] = "$"
+  end
 end
-new_array
-end 
+
+def find_a(array)
+  array.find_all do |word|
+    word[0] == "a"
+  end
+end
+
+def sum_array(array)
+  array.inject(0) {|sum, i| sum + i}
+end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+end
+end
+end
