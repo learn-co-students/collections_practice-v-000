ca306783e09cbc348c226e0afb7f1e7164213706
Daksha Patel <dakshapatel100@gmail.com>
2018-05-04 19:37:45 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,31 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+
+def sort_array_desc(array)
+  array.sort {|x,y| y <=> x}
+end
+
+
+def sort_array_char_count(array)
+  array.sort {|x, y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+  array[0], array [1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  counter = 1
+  new_array= []
+  array.each do {|dollar_sign| array [2]}
+    counter += 1
+end
+new_array
+end 
