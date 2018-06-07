3268447052764a6b4b22247937d5a9b4a0be6b89
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-06-07 17:44:28 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,10 +6,14 @@ def sort_array_desc(array)
   array.sort.reverse
 end
 
-#def sort_array_char_count(array)
-#  array.sort {}
-#end
+def sort_array_char_count(array)
+  array.sort_by {|character| character.length}
+end
 
 def swap_elements(array, index, desintation_index)
-  array.swap([1, 2],2,1)
+  array[1], array[2] = array[2], array[1]
+end
+
+def reverse_array(array)
+  return array.reverse
 end
