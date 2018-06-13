05afc896615d0b3dbd6c7e8f970cc240e98446dc
Cristina Lugo <cmlugoce@syr.edu>
2018-06-13 02:35:18 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,70 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b 
+ end 
+end 
 
+def sort_array_desc(array)
+  array.sort do |a, b| 
+    b <=> a 
+  end 
+end
+
+
+def sort_array_char_count(array) 
+  array.sort do |a, b|
+    if a.length == b.length
+      0 
+    elsif a.length < b.length
+      -1 
+    elsif a.length > b.length
+      1
+  end 
+end 
+end 
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+ return array 
+end 
+
+#advanced question 
+def swap_elements_from_to (array, index, new_index)
+  array[index], array[new_index] = array[new_index], array[index]
+return array 
+end 
+
+def reverse_array(array_integers)
+  array_integers.reverse
+end 
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$"
+  end 
+end 
+
+def find_a(array)
+  array.find_all do |word|
+    word.start_with? ("a")
+  end 
+end 
+
+def sum_array(array)
+  array.inject do |sum, integer|
+    sum + integer 
+  end 
+end 
+
+def add_s(array)
+  array.each_with_index.collect do |word, index|
+    if index == 1 
+      array[index]
+    else 
+      word << "s"
+    end 
+  end 
+  end 
+  
+  
+    
