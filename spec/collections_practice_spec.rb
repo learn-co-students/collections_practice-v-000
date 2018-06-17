85ee09eafed350fb5f151d6480e792d3fae5ed0b
Karyme Virginia <aveonyxcode@gmail.com>
2018-06-17 14:06:02 -0700

    Done.

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,13 @@ describe 'collections practice' do
     end
   end
 
+  # Question 4 Bonus
+  describe '#swap_elements_from_to' do
+    it 'swaps the elements of the given indeces in an array' do
+      expect(swap_elements_from_to(["blake", "ashley", "scott", "gregory"], 0, 3)).to eq(["gregory", "ashley", "scott", "blake"])
+    end
+  end
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array of integers' do
