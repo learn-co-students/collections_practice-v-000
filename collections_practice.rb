ace046d7b4a3bc63fb87b7f6534007aba4d98e3b
Daniel Etter <etter.dt@gmail.com>
2018-06-28 21:37:29 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,51 @@
+require "pry"
 
+def sort_array_asc(asc)
+  asc.sort
+end
+
+def sort_array_desc(desc)
+  desc.sort.reverse
+end
+
+def sort_array_char_count(count)
+  count.sort{|a, b| a.length <=> b.length}
+end
+
+def swap_elements(elements)
+  elements[1], elements[2] = elements[2], elements[1]
+  elements
+end
+
+def reverse_array(rev)
+  rev.reverse
+end
+
+def kesha_maker(kesha)
+  kesha.each do |replace|
+    #binding.pry
+    replace[2]="$"
+  end
+end
+
+def find_a(searching)
+  searching.select do |word|
+    word.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject(0) do |result, element| 
+    result = result + element 
+  end 
+end
+
+def add_s(additions)
+    additions.collect do |word|
+      if word != additions[1]
+         word + "s"
+      else
+        word
+      end
+    end
+end  
