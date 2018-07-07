5c9ea48708fd3769b797baf3af03ca1e3b46e6bd
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-07-07 13:27:26 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
+def sort_array_asc(integers)
+  integers.sort
+end
 
+def sort_array_desc(integers)
+  integers.sort.reverse
+end
+
+def sort_array_char_count(stuff)
+  stuff.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(stuff)
+  stuff.sort do |a, b|
+    a[-1] <=> b[-2]
+  end
+end
+
+def reverse_array(integers)
+  integers.reverse
+end
+
+def kesha_maker(names)
+  names.map! do |x|
+    x[2] << "$"
+  end
+end
+
+def find_a(words)
+  words.start_with?("a")
+end
