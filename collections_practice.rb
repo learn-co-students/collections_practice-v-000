e8a55c4a9a03fa64ce61fa34b07a53d7562a6bdb
Kristen Papke <klpapke215@gmail.com>
2018-07-15 19:14:05 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
+def sort_array_asc(integers)
+  integers.sort
+end
 
+def sort_array_desc(integers)
+  integers.sort.reverse
+end
+
+def sort_array_char_count(strings)
+  strings.sort_by(&:length)
+end
+
+def swap_elements(array)
+  array [0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |word|
+    word[2] = "$"
+    new_array << word
+  end
+  new_array
+end
+
+def find_a(array)
+  array.select do |strings|
+    strings.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  sum = 0
+  array.each do |a|
+    sum += a
+  end
+  sum
+end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
