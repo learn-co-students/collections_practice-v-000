732584ebb7348161de7b530c9669fe04b2d0bdc0
Anna Dybas <a.m.dybas@gmail.com>
2018-07-17 18:50:46 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|a,b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a,b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array.sort {|a,b| a[1] <=> b[2]}
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array) #how?
+  array.each {|dollar_sign| dollar_sign[2] = "$"}
+end
+
+def find_a(array)
+  array.select do |word|
+    word.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject {|sum, n| sum + n}
+end
+
+def add_s(array)
+  add_s.each_with_index.collect {|plural, index| plural + "s" index}
+end
