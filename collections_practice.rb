7f04f062699bcb7cee27bdee21aec5dd63d18b7c
Karen C <chenyanhuan66@163.com>
2018-07-30 06:08:48 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+def sort_array_asc(array)
+  return array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a,b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |i|
+    i[2]="$"
+  end
+end
+
+def find_a(array)
+  array2 = []
+  array.each do |i|
+    if i[0] == "a"
+      array2 << i
+    end
+  end
+  return array2
+end
+
+def sum_array(array)
+  sum = 0
+  array.each {|a| sum += a}
+  return sum
+end
+
+def add_s(array)
+  array.each do |i|
+    i << "s"
+  end
+  array[1][array[1].length - 1] = ""
+  return array
+end
