f70e5f31bef86297d8591bcf50a6bf3043630e89
Brad McCollum <btmcco8@gmail.com>
2018-08-14 16:38:51 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,62 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a.length > b.length
+      1
+    elsif a.length < b.length
+      -1
+    end
+  end
+end
+
+def swap_elements(array)
+  new_array = []
+  array.each { |x| new_array << x }
+  new_array[1], new_array [2] = new_array[2], new_array[1]
+  new_array
+end
+
+def swap_elements_from_to(array, index, destination)
+    array[index], array[destination] = array[destination], array[index]
+    return array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.collect do |x|
+    x[2] = "$"
+    x
+  end
+end
+
+def find_a(array)
+  array.delete_if { |x| x.start_with?("a") == false } #alt: array.reject {|x| x.start_with?("a") == false}
+end
+
+def sum_array(array)
+  array.inject { |sum, n| sum + n }
+end
+
+def add_s(array)
+  array.each_with_index.collect {|x, index| index != 1 ? x + "s" : x }
+end
