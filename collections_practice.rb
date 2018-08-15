4790f2681053aef8397ab3f11a530d1178e62e4f
Steven Crouse <sdcrouse91@gmail.com>
2018-08-15 08:01:42 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,25 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b 
+      0 
+    elsif a < b
+      1
+    else # a > b
+      -1
+    end
+  end
+  
+  # A faster way:
+  #array.sort {|a, b| b <=> a}
+  
+  # An even faster way:
+  # array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.length <=> b.length}
+end
