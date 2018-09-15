f2df7a55553932fd7d4e05e4346f2ff8c38fabc6
Tim Knoll <timothy.knoll@gmail.com>
2018-09-15 03:17:32 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+def sort_array_asc (integers)
+  integers.sort
+end
 
+def sort_array_desc (integers)
+  integers.sort { |a,b| b <=> a}
+end
+
+def sort_array_char_count (strings)
+  strings.sort { |left, right| left.length <=> right.length }
+end
+
+def swap_elements (array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array (array)
+  return array.reverse
+end
+
+def kesha_maker (array)
+  kesha = []
+  array.each do |word|
+    word_array = word.split ""
+    word_array[2] = "$"
+    kesha << word_array.join
+  end
+  return kesha
+end
+
+def find_a (array)
+  array.select {|string| string.start_with?("a")}
+end
+
+def sum_array (integers)
+  integers.inject {|n, sum| sum + n}
+end
+
+def add_s (array)
+  array.each_with_index.collect do |string, index|
+    if index == 1
+      string
+    else
+      string << "s"
+    end
+  end
+end  
