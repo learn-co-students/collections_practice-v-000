c14f83e1483670cd991df17d41e6e39d1c9845f2
Paul Fell <swellpf@gmail.com>
2018-09-26 20:36:42 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,7 +11,8 @@ def sort_array_char_count(strings)
 end
 
 def swap_elements(array)
- # array.sort {|a,b| array[2] <=> array[0]}
+ array[1], array[2] = array[2], array[1]
+    array
 end
 
 def reverse_array(integers)
@@ -20,17 +21,22 @@ end
 
 # Question 6
 def kesha_maker(strings)
-  
+  kesha_strings = []
+  strings.each do |string|
+    string.sub!(string.slice(2),"$")
+    kesha_strings << string
+  end
+    kesha_strings
 end
 
 # Question 7
-def find_a(args)
-  
+def find_a(strings)
+  strings.select {|string| string.start_with?("a")}
 end
 
 # Question 8
-def sum_array
-  
+def sum_array(integers)
+  integers.inject {|sum, n| sum + n}
 end
 
 # Question 9 - Add_s
