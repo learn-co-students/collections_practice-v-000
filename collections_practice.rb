7ee081c5b1d3195a0bc6c3d82027c9bfdf82c4b1
rosa lee <rosalee0116@gmail.com>
2018-10-03 17:15:10 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(integers)
+  integers.sort
+end
 
+def sort_array_desc(integers)
+  integers.sort {|a,b| b<=>a}
+end
+
+def sort_array_char_count(strings)
+  strings.sort {|a,b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(integers)
+  integers.reverse
+end
+
+#6
+def kesha_maker(strings)
+  kesha_strings = []
+  strings.each do |string|
+    string.sub!(string.slice(2), "$")
+    kesha_strings << string
+  end
+  kesha_strings
+end
+
+#7
+def find_a(strings)
+  strings.select {|string| string.start_with?("a")}
+end
+
+#8
+def sum_array(integers)
+  integers.inject{|sum, n| sum + n}
+end
+
+#9
+def add_s(array)
+  array.each_with_index.collect{|element,index| index !=1 ? element +"s" : element}
+end
