cee89a6cd6ef3b192cfa9438df106897dd2b1bd3
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-10-04 18:23:07 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,25 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort! do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  second = array[1]; third = array[2]
+  array[1] = third; array[2] = second
+  array
+end
+
+#Question 4b ********
+def swap_elements_from_to()
+
+#Question 4b ********
