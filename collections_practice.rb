d52fd73dc4c887ae7a91abe617c490a8101c6e03
Kimberly Aviles <01010110.v@gmail.com>
2018-11-13 12:28:38 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,30 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort do |smaller, larger|
+  larger <=> smaller
+  end
+end
+
+def sort_array_char_count(array_of_strings)
+  array_of_strings.sort do |a, b|
+  a.downcase.length <=> b.downcase.length
+  end
+end
+
+def swap_elements(array)
+  array.sort do |a, b|
+    if a == array[1] && b == array[2]
+      1
+    else
+      0
+    end
+  end
+end
+
+def reverse_array(array)
+  array.reverse
+end
