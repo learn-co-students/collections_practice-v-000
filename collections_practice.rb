05ddd8c76a4dc32c89ed9f1b1cc08018b8e0967c
Toni-Ty <antonique2479@yahoo.com>
2019-02-02 23:47:43 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,3 +11,28 @@ def sort_array_asc(array)
   a <=> b
   end
 end
+
+
+
+
+
+
+
+# def sort_array_desc(array) this method also works, but is longer
+#
+# array.sort do |a, b|
+#   b <=> a
+#   end
+# end
+
+
+def sort_array_desc(array)
+
+array.sort { |a, b|   b <=> a} #this method block eliminates do and end, and is more elegant
+
+end
+
+def sort_array_char_count (array)
+  array.sort { |a|   a.length}
+end
+  
