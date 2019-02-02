576837b1868320addd85486ed1c5231fca50e09c
Toni-Ty <antonique2479@yahoo.com>
2019-02-02 23:09:08 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,12 @@
+# dishes = ["steak", "apple pie", "vegetable soup"]
+#
+# dishes.sort do |a, b|
+#     a <=> b
+# end
+#
+array = [25, 7, 1]
 
+def sort_array_asc
+  array.sort do |a, b|
+  a <=> b
+end
