8098eb125ae6bcb480c2b92ef9b22ad92eefbaeb
Dan Suarez <suarez.daniel101@gmail.com>
2019-02-26 01:12:12 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,21 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  #if array[1] && array[2]
+  array.sort {|a, b| a[1] <=> b[2]}
+  #end
+end
+
+def reverse_array(array)
+  array.reverse
+end
