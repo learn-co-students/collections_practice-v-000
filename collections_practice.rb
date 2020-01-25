f258b62798f99b5abc4a65f01b8b1967318edf94
Julie  McManus <julieannesays@gmail.com>
2020-01-25 22:33:25 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,58 +1,41 @@
 def sort_array_asc(array)
-	array.sort
+	array.sort { |a, b| a <=> b}
 end
-
 
 def sort_array_desc(array)
-	array.sort.reverse
+	array.sort { |a, b| b <=> a}
 end
-
 
 def sort_array_char_count(array)
-	array = array.sort_by do |x| x.length
-	end
+	array.sort { |a, b| a.length <=> b.length}
 end
-
 
 def swap_elements(array)
 	array[0], array[1], array[2] = array[0], array[2], array[1]
 end
 
-
 def reverse_array(array)
 	array.reverse
 end
 
-
 def kesha_maker(array)
-  kesha_words = []
-  array.each do |word| word[2] = "$"
-    kesha_words << word
-  end
-  kesha_words
+	array.each {|word| word[2]= "$"}
 end
-
 
 def find_a(array)
-	array.select do |word| word.start_with?("a")
-	end
+	array.select {|word| word.start_with?("a")}
 end
-
 
 def sum_array(array)
-	array.inject(0) do |x, y| y + x
-	end
+	array.sum
 end
 
-
 def add_s(array)
-	array.collect do |word|
-		if array[1] == word
+	array.each_with_index.collect do |word, index|
+		if index == 1
 			word
 		else
 			word + "s"
 		end
 	end
-
-
 end
